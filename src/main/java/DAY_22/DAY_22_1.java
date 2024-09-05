35221870867e87680d8e7a42c585475ad964770a
pawelprimus <pawelprimus@gmail.com>
2024-09-05 19:23:51 +0200

    Refactored 22_1

--- src/main/java/DAY_22/DAY_22_1.java
+++ src/main/java/DAY_22/DAY_22_1.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class DAY_22_1 {
 
@@ -22,10 +21,10 @@ public class DAY_22_1 {
             allBricks.add(createBrick(input[i], i + 65));
         }
 
-        for (Brick brick : allBricks) {
-            brick.print();
-            System.out.println(brick.xPoints().stream().map(String::valueOf).collect(Collectors.joining(", ")));
-        }
+//        for (Brick brick : allBricks) {
+//            brick.print();
+//            System.out.println(brick.xPoints().stream().map(String::valueOf).collect(Collectors.joining(", ")));
+//        }
 
         boolean isAnythingChanged = true;
         while (isAnythingChanged) {
@@ -38,14 +37,13 @@ public class DAY_22_1 {
         }
 
         for (Brick brick : allBricks) {
-            brick.attachSuporting(allBricks);
+            brick.attachConnectedBricks(allBricks);
         }
         int result = 0;
         for (Brick brick : allBricks) {
-            if(brick.canBeRemoved()){
+            if (brick.canBeRemoved()) {
                 result++;
             }
-            System.out.println(brick.getCharId() + " " + brick.canBeRemoved());
         }
 
         System.out.println("RESULT " + result); // 499
@@ -72,8 +70,10 @@ class Brick {
     private int yEnd;
     private int zStart;
     private int zEnd;
-    private List<Brick> supportedBricks = new ArrayList<>();
-    private List<Brick> supportedBy = new ArrayList<>();
+    // Bricks that are connected above this brick
+    private List<Brick> upConnectedBricks = new ArrayList<>();
+    // Bricks that are connected under this brick
+    private List<Brick> downConnectedBricks = new ArrayList<>();
 
     public Brick(int xStart, int xEnd, int yStart, int yEnd, int zStart, int zEnd, int id) {
         this.xStart = xStart;
@@ -87,17 +87,16 @@ class Brick {
     }
 
     public boolean canBeRemoved() {
-        for (Brick supportedBrick : supportedBricks) {
-            if(supportedBrick.hasLessThan2Supporters()){
+        for (Brick supportedBrick : upConnectedBricks) {
+            if (supportedBrick.hasLessThan2Supporters()) {
                 return false;
             }
         }
-
         return true;
     }
 
-    public boolean hasLessThan2Supporters() {
-        return supportedBy.size() < 2;
+    private boolean hasLessThan2Supporters() {
+        return downConnectedBricks.size() < 2;
     }
 
     public boolean moveBrickIfPossible(List<Brick> bricks) {
@@ -108,125 +107,117 @@ class Brick {
         return false;
     }
 
-    public void attachSuporting(List<Brick> bricks) {
-        for (Brick brick : bricks) {
-            // check if is brick lower by on Z axis
-            if (isBrickLevelHigher(brick)) {
-                // check if brick has any interfere in X or Y axis
-                if (isBrickInterfereXorYaxis(brick)) {
-                    addSupportedBrick(brick);
-                    brick.addSupportedByBrick(this);
-                }
-            }
-        }
-    }
-
     public boolean canMoveLower(List<Brick> bricks) {
         if (getzStart() == 1) {
             return false;
         }
         for (Brick brick : bricks) {
             // check if is brick lower by on Z axis
-            if (isBrickLevelLower(brick)) {
-                brick.print();
-                System.out.println("is level lower to ");
-                this.print();
-                System.out.println();
-                // check if brick has any interfere in X or Y axis
-                if (isBrickInterfereXorYaxis(brick)) {
-                    return false;
-                }
+            if (isInterfereOnDownEdge(brick)) {
+                return false;
             }
         }
         return true;
     }
 
     private void moveLower() {
-        System.out.println("move brick lower id[" + charId + "]" + " current z's [" + zStart + " -> " + zEnd + "]");
-        this.zStart = zStart - 1;
-        this.zEnd = zEnd - 1;
+        this.zStart--;
+        this.zEnd--;
     }
 
-    public boolean isBrickInterfereXorYaxis(Brick brick) {
-        return isBrickInterfereX(brick) && isBrickInterfereY(brick);
-    }
-
-    public boolean isBrickInterfereX(Brick brick) {
-        for (Integer thisBrick : xPoints()) {
-            if (brick.xPoints().contains(thisBrick)) {
-                return true;
+    public void attachConnectedBricks(List<Brick> bricks) {
+        for (Brick brick : bricks) {
+            // check if is brick lower by on Z axis
+            if (isBrickLevelHigher(brick)) {
+                // check if brick has any interfere in X or Y axis
+                if (isBrickInterfereXorYaxis(brick)) {
+                    addUpConnectedBrick(brick);
+                    brick.addDownConnectedBrick(this);
+                }
             }
         }
-        return false;
     }
 
-    public boolean isBrickInterfereY(Brick brick) {
-        for (Integer thisBrick : yPoints()) {
-            if (brick.yPoints().contains(thisBrick)) {
-
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public boolean isBrickLevelLower(Brick brick) {
-        return getzStart() - brick.getzEnd() == 1;
-    }
-
-    public boolean isBrickLevelHigher(Brick brick) {
+    private boolean isBrickLevelHigher(Brick brick) {
         return getzEnd() - brick.getzStart() == -1;
     }
 
-    private void addSupportedBrick(Brick brick) {
-        if (!supportedBricks.contains(brick)) {
-            supportedBricks.add(brick);
+    private boolean isInterfereOnDownEdge(Brick brick) {
+        return isBrickLevelLower(brick) && isBrickInterfereXorYaxis(brick);
+    }
+    private boolean isBrickInterfereXorYaxis(Brick brick) {
+        return isBrickInterfereX(brick) && isBrickInterfereY(brick);
+    }
+
+    private boolean isBrickInterfereX(Brick brick) {
+        // x x 1 2 3 4 x x x x
+        // x x x x 3 4 5 6 7 x
+        boolean interfereRightToLeft = this.getxStart() <= brick.getxStart() && this.getxEnd() >= brick.getxStart();
+        // x x x x 3 4 5 6 7 x
+        // x x 1 2 3 4 x x x x
+        boolean interfereLeftToRight = this.getxStart() >= brick.getxStart() && this.getxStart() <= brick.getxEnd();
+        return interfereRightToLeft || interfereLeftToRight;
+    }
+
+    private boolean isBrickInterfereY(Brick brick) {
+        boolean interfereRightToLeft = this.getyStart() <= brick.getyStart() && this.getyEnd() >= brick.getyStart();
+        boolean interfereLeftToRight = this.getyStart() >= brick.getyStart() && this.getyStart() <= brick.getyEnd();
+        return interfereRightToLeft || interfereLeftToRight;
+    }
+
+    private boolean isBrickLevelLower(Brick brick) {
+        return getzStart() - brick.getzEnd() == 1;
+    }
+
+    private void addUpConnectedBrick(Brick brick) {
+        if (!upConnectedBricks.contains(brick)) {
+            upConnectedBricks.add(brick);
         }
     }
 
-    private void addSupportedByBrick(Brick brick) {
-        if (!supportedBy.contains(brick)) {
-            supportedBy.add(brick);
+    private void addDownConnectedBrick(Brick brick) {
+        if (!downConnectedBricks.contains(brick)) {
+            downConnectedBricks.add(brick);
         }
     }
 
-    // Z start is always smaller
-    public int getzStart() {
+    // Z start is always smaller than Z end
+    private int getzStart() {
         return zStart;
     }
 
-    public int getzEnd() {
+    private int getzEnd() {
         return zEnd;
     }
 
-    public List<Integer> xPoints() {
-        return points(xStart, xEnd);
+    public int getxStart() {
+        return xStart;
     }
 
-    public List<Integer> yPoints() {
-        return points(yStart, yEnd);
+    public int getxEnd() {
+        return xEnd;
     }
 
-    public List<Integer> zPoints() {
-        return points(zStart, zEnd);
+    public int getyStart() {
+        return yStart;
     }
 
-    private List<Integer> points(int start, int end) {
-        return IntStream.rangeClosed(start, end).boxed().toList();
+    public int getyEnd() {
+        return yEnd;
     }
 
 
-    public String supported() {
-        return supportedBricks.stream().map(i -> i.charId).map(String::valueOf).collect(Collectors.joining(", "));
+    public String upConnected() {
+        return upConnectedBricks.stream().map(i -> i.charId).map(String::valueOf).collect(Collectors.joining(", "));
     }
 
-    public String supporting() {
-        return supportedBy.stream().map(i -> i.charId).map(String::valueOf).collect(Collectors.joining(", "));
+    public String downConnected() {
+        return downConnectedBricks.stream().map(i -> i.charId).map(String::valueOf).collect(Collectors.joining(", "));
     }
 
     public void print() {
         System.out.println(charId + " X[" + xStart + " -> " + xEnd + "] Y [" + yStart + " -> " + yEnd + "] Z [" + zStart + " -> " + zEnd + "]"
-                + " supported [" + supported() + "] supportedBy[" + supporting() + "]");
+                + " supported [" + upConnected() + "] supportedBy[" + downConnected() + "]");
     }
 
     public char getCharId() {
